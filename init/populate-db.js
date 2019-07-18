f1fc0a4d3218b44cc0822844a8dbcd6612394d2f
Jagrajan Bhullar <jag@jagrajan.com>
2019-07-18 05:00:06 +0000

    Add pumpkin alfredo default recipe

--- init/populate-db.js
+++ init/populate-db.js
@@ -27,6 +27,11 @@ const populate = async () => {
   const red_pepper_flakes_id = await createIngredient('red pepper flakes', 'red pepper flakes');
   const salt_and_pepper_id = await createIngredient('salt and pepper', 'salt and pepper');
   const minced_garlic_id = await createIngredient('minced garlic', 'minced garlic');
+  const large_shell_pasta_id = await createIngredient('large shell pasta', 'large shell pasta');
+  const flour_id = await createIngredient('flour', 'flour');
+  const margarine_id = await createIngredient('margarine', 'margarine');
+  const half_and_half_id = await createIngredient('half and half', 'half and half');
+  const pumpkin_puree_id = await createIngredient('pumpkin purée', 'pumpkin purée');
 
   const no_unit_id = await createUnit('', '');
   const cup_id = await createUnit('cup', 'cups');
@@ -35,6 +40,95 @@ const populate = async () => {
   const hf_id = await createUnit('handful', 'handfuls');
   const pinch_id = await createUnit('pinch', 'pinches');
   const ml_id = await createUnit('can', 'cans');
+  const gram_id = await createUnit('gram', 'grams');
+
+  const pumpkin_alfredo = await createRecipe('pumpkin-alfredo');
+  const pumpkin_alfredo_version = await createRecipeVersion(
+    pumpkin_alfredo.id,
+    pumpkin_alfredo.latest_version,
+    'Pumpkin Alfredo',
+    'Very fast, but tasty pasta',
+    `p Just don't add too much salt!`,
+    [
+      {
+        position: 1,
+        description: 'Cook shell pasta according to directions. Strain pasta and set aside.'
+      },
+      {
+        position: 2,
+        description: 'Heat margarine in large pot over medium heat.'
+      },
+      {
+        position: 3,
+        description: 'Add minced garlic to margarine and stir until garlic is fragrant. Do not burn.'
+      },
+      {
+        position: 4,
+        description: 'Whisk in flour until thin paste forms. Cook for ~ 1 minute.'
+      },
+      {
+        position: 5,
+        description: 'Whisk in pumpkin and half and half. Simmer on low-meidum heat until sauce thickens slightly.'
+      },
+      {
+        position: 6,
+        description: 'Stir in Parmesan and heat until cheese is melted.'
+      },
+      {
+        position: 7,
+        description: 'Add pasta to sauce and toss until hot.'
+      },
+      {
+        position: 8,
+        description: 'Serve immediately, top with additional Parmesan.'
+      }
+    ],
+    [
+      {
+        position: 1,
+        ingredient: 'large shell pasta',
+        minAmount: 350,
+        maxAmount: 350,
+        unit: gram_id
+      },
+      {
+        position: 2,
+        ingredient: 'margarine',
+        minAmount: 2,
+        maxAmount: 2,
+        unit: tbsp_id
+      },
+      {
+        position: 3,
+        ingredient: 'minced garlic',
+        minAmount: 3,
+        maxAmount: 3,
+        unit: tbsp_id
+      },
+      {
+        position: 4,
+        ingredient: 'flour',
+        minAmount: 1,
+        maxAmount: 2,
+        unit: tbsp_id
+      },
+      {
+        position: 5,
+        ingredient: 'pumpkin purée',
+        minAmount: 1,
+        maxAmount: 1,
+        unit: cup_id
+      },
+      {
+        position: 6,
+        ingredient: 'parmesan',
+        minAmount: '1/2',
+        maxAmount: '1/2',
+        unit: cup_id
+      }
+    ],
+    'more_fud.jpg'
+  );
 
   // Creamy tomato pasta
   const tomato_spinach_pasta = await createRecipe('tomato-spinach-pasta');
@@ -95,7 +189,7 @@ const populate = async () => {
         minAmount: 6,
         maxAmount: 6,
         position: 2,
-        unit: tbsp_id 
+        unit: tbsp_id
       },
       {
         ingredient: 'medium onion',
@@ -116,68 +210,69 @@ const populate = async () => {
         minAmount: 4,
         maxAmount: 4,
         position: 5,
-        unit: tbsp_id 
-      }, 
+        unit: tbsp_id
+      },
       {
         ingredient: 'cream cheese',
         minAmount: 6,
         maxAmount: 6,
         position: 6,
-        unit: tbsp_id 
-      }, 
+        unit: tbsp_id
+      },
       {
         ingredient: 'fresh or frozen spinach',
         minAmount: 2,
         maxAmount: 2,
         position: 7,
         unit: hf_id
-      }, 
+      },
       {
         ingredient: 'parmesan',
         minAmount: '1/2',
         maxAmount: '1/2',
         position: 8,
         unit: cup_id
-      }, 
+      },
       {
         ingredient: 'dried oregano',
         minAmount: 2,
         maxAmount: 2,
         position: 9,
         unit: tsp_id
-      }, 
+      },
       {
         ingredient: 'dried basil',
         minAmount: 2,
         maxAmount: 2,
         position: 10,
-        unit: tsp_id 
-      }, 
+        unit: tsp_id
+      },
       {
         ingredient: 'red pepper flakes',
         minAmount: 1,
         maxAmount: 1,
         position: 11,
-        unit: pinch_id 
-      }, 
+        unit: pinch_id
+      },
        {
         ingredient: 'salt and pepper',
         minAmount: '',
         maxAmount: '',
         position: 12,
-        unit: no_unit_id 
-      }, 
+        unit: no_unit_id
+      },
        {
         ingredient: 'minced garlic',
         minAmount: 2,
         maxAmount: 2,
         position: 13,
-        unit: tbsp_id 
-      }, 
-        
+        unit: tbsp_id
+      },
     ],
     'bibsy-lies-down.png'
   );
+
+
 
 }
 
